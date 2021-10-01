3895807a029e0a36d1f7201e0ab03d4bb693e8f8
Lixing <lixing.sun@thoughtworks.com>
2021-10-01 13:24:15 +0800

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          GATE OF BABYLON
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
