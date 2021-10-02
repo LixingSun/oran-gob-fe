cb523d09860e9b812c872ac1197244e37b8d823e
Lixing <lixing.sun@thoughtworks.com>
2021-10-02 17:29:14 +0800

    Rotate camera as cursor movement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import {Canvas} from "@react-three/fiber";
-import {Cylinder, OrbitControls, PerspectiveCamera} from "@react-three/drei";
+import {useState} from "react";
+import {Canvas, useFrame, useThree} from "@react-three/fiber";
+import {Cylinder, PerspectiveCamera, OrbitControls} from "@react-three/drei";
 
 const cylinderConfigs = [
   {
@@ -30,19 +31,38 @@ const cylinderConfigs = [
   }
 ]
 
+const AnimateCamera = ({cursorPosition, screenSize}) => {
+  const { gl, camera } = useThree()
+
+  const animationRatio = Math.PI / 2 / 90 * 0.5
+
+  const calculateDegree = (screenSize, current) => {
+    const percentage = (screenSize / 2  - current) / screenSize * 2;
+    return percentage * animationRatio
+  }
+
+  useFrame(() => {
+    camera.rotateX(calculateDegree(screenSize[1], cursorPosition[1]));
+    camera.rotateY(calculateDegree(screenSize[0], cursorPosition[0]));
+  });
+
+  return <OrbitControls enablePan={false} enableRotate={false} enableZoom={false}
+                        target={[0, 0, 0]} args={[camera, gl.domElement]}/>;
+}
+
 const App = () => {
+  const [cursorPosition, setCursorPosition] = useState([null, null]);
+  const screenSize = [window.innerWidth, window.innerHeight];
+
   return (
-    <div className="App">
-      <Canvas>
-        <color attach="background" args={"#212121"} />
-        {/*<axesHelper />*/}
+    <div className="App" onMouseMove={event => setCursorPosition([event.clientX, event.clientY])}>
+      <Canvas onCreated={state => state.gl.setClearColor("#212121")}>
         <PerspectiveCamera makeDefault position={[0, 0, 5]}
                            fov={50}
                            aspect={window.innerWidth/window.innerHeight}
                            near={0.1}
                            far={1000} />
         <directionalLight position={[5, 10, 25]} intensity={1} color={"#FFFFFF"}/>
-        <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
 
         { cylinderConfigs.map((cylinderConfig, index) => (
               <Cylinder args={[1, 1, 0.1, 6]} key={index} position={[
@@ -52,6 +72,7 @@ const App = () => {
               </Cylinder>
           )) }
 
+        <AnimateCamera cursorPosition={cursorPosition} screenSize={screenSize}/>
       </Canvas>
     </div>
   );
