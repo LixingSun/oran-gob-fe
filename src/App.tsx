c6d30721419d8fe6fc54dcff628c82f433c1ac39
Lixing <lixing.sun@thoughtworks.com>
2021-10-06 22:02:51 +0800

    Modify landing animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import React, { useState, FC } from "react";
-import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
-import { PerspectiveCamera, OrbitControls, Text } from "@react-three/drei";
+import React, { FC } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Text, Stats } from "@react-three/drei";
 import { animated, SpringRef, SpringValue } from "@react-spring/three";
 import { useSprings } from "@react-spring/core";
 import cnFont from "./fonts/cn-font.ttf";
-import { easeCubicOut } from "d3-ease";
+import { easeCubicOut, easeCubicInOut } from "d3-ease";
+import { PerspectiveCamera } from "three";
 
 const data: string[] = ["前端", "后端", "DevOps", "Me", "架构", "测试", "游戏"];
 
@@ -53,45 +54,45 @@ const tileConfigs: ITileConfig[] = [
   },
 ];
 
-type TypeCursorPosition = [number, number];
-type TypeScreenSize = [number, number];
-
 interface AnimateCameraProps {
-  cursorPosition: TypeCursorPosition;
-  screenSize: TypeScreenSize;
+  cameraZ: SpringValue<number>;
 }
 
-const AnimateCamera: FC<AnimateCameraProps> = ({
-  cursorPosition,
-  screenSize,
-}) => {
-  const { gl, camera } = useThree();
+const AnimateCamera: FC<AnimateCameraProps> = ({ cameraZ }) => {
+  const { camera, set, size } = useThree();
+  const cameraRef = React.useRef<PerspectiveCamera>();
 
-  const animationRatio = (Math.PI / 2 / 90) * 10;
+  React.useLayoutEffect(() => {
+    const { current: cam } = cameraRef;
+    if (cam) {
+      cam.aspect = size.width / size.height;
+      cam.updateProjectionMatrix();
+    }
+  }, [size]);
 
-  const calculateDegree = (screenSize: number, current: number): number => {
-    const percentage = ((screenSize / 2 - current) / screenSize) * 2;
-    return percentage * animationRatio;
-  };
-
-  useFrame(() => {
-    camera.rotateX(calculateDegree(screenSize[1], cursorPosition[1]));
-    camera.rotateY(calculateDegree(screenSize[0], cursorPosition[0]));
-  });
+  React.useLayoutEffect(() => {
+    if (cameraRef.current) {
+      const oldCam = camera;
+      set(() => ({ camera: cameraRef.current! }));
+      return () => set(() => ({ camera: oldCam }));
+    }
+  }, [camera, cameraRef, set]);
 
   return (
-    <OrbitControls
-      enablePan={false}
-      enableRotate={false}
-      enableZoom={false}
-      target={[0, 0, 0]}
-      args={[camera, gl.domElement]}
+    <animated.perspectiveCamera
+      ref={cameraRef}
+      position-x={0}
+      position-y={0}
+      position-z={cameraZ}
+      fov={50}
+      aspect={window.innerWidth / window.innerHeight}
+      near={0.1}
+      far={1000}
     />
   );
 };
 
 interface TileProps {
-  font: any;
   tileConfig: ITileConfig;
   tileIndex: number;
   animationApi: SpringRef<{ z: number; textZ: number }>;
@@ -153,22 +154,18 @@ const Tile: FC<TileProps> = ({
   );
 };
 
-const landingDuration = 1200;
+const landingDuration = 2400;
 
 const App: FC = () => {
-  const [cursorPosition, setCursorPosition] = useState<TypeCursorPosition>([
-    null,
-    null,
-  ]);
-
-  const screenSize: TypeScreenSize = [window.innerWidth, window.innerHeight];
-
   const [hoverAnimationSprings, hoverAnimationApi] = useSprings(
     data.length,
     () => ({
       z: 0,
       textZ: 2,
-      config: { mass: 10, tension: 1000, friction: 300, precision: 0.00001 },
+      config: {
+        duration: 700,
+        easing: easeCubicInOut,
+      },
     })
   );
 
@@ -177,6 +174,7 @@ const App: FC = () => {
     (index) => ({
       x: tileConfigs[index].x * 10,
       y: tileConfigs[index].y * 10,
+      cameraZ: 80,
       config: {
         duration: landingDuration,
         easing: easeCubicOut,
@@ -185,19 +183,9 @@ const App: FC = () => {
   );
 
   return (
-    <div
-      className="App"
-      onMouseMove={(event) => setCursorPosition([event.clientX, event.clientY])}
-    >
+    <div className="App">
       <Canvas onCreated={(state) => state.gl.setClearColor("#212121")}>
-        <PerspectiveCamera
-          makeDefault
-          position={[0, 0, 110]}
-          fov={50}
-          aspect={window.innerWidth / window.innerHeight}
-          near={0.1}
-          far={1000}
-        />
+        <Stats showPanel={0} className="stats" />
         <directionalLight
           position={[30, 40, 70]}
           intensity={1}
@@ -215,21 +203,19 @@ const App: FC = () => {
               z={hoverAnimationSprings[tileIndex]?.z}
               textZ={hoverAnimationSprings[tileIndex]?.textZ}
               onClick={() => {
-                tileIndex === 3
-                  ? landingAnimationApi.start((animatedIndex) => ({
-                      x: tileConfigs[animatedIndex].x,
-                      y: tileConfigs[animatedIndex].y,
-                    }))
-                  : null;
+                if (tileIndex === 3) {
+                  landingAnimationApi.start((animatedIndex) => ({
+                    x: tileConfigs[animatedIndex].x,
+                    y: tileConfigs[animatedIndex].y,
+                    cameraZ: 120,
+                  }));
+                }
               }}
             />
           </animated.group>
         ))}
 
-        <AnimateCamera
-          cursorPosition={cursorPosition}
-          screenSize={screenSize}
-        />
+        <AnimateCamera cameraZ={landingAnimationSprings[0].cameraZ} />
       </Canvas>
     </div>
   );
